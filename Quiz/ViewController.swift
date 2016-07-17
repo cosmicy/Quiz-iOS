55ea2753605e19ada3065618d6487e075e606dd8
cymac <cosmicy@outlook.com>
2016-07-17 21:49:55 +0800

    *添加action

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,5 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel:UILabel!
     @IBOutlet var answerLabel:UILabel!
     
+    let question : [String] = ["q1sdf","q2flskd","q3ldskjf"]
+    let answer: [String] = ["123", "456", "789"]
+    var currentQuestionIndex : Int = 0
+    
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+    }
+    
 }
 
