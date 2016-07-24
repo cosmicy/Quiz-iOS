85acf172d1cc31eda2acc5c14e91fcc99bbe97e9
cymac <cosmicy@outlook.com>
2016-07-24 21:12:50 +0800

    *学习变量、向量、字典的定义

--- playground/MyPlayground.playground/Contents.swift
+++ playground/MyPlayground.playground/Contents.swift
@@ -3,3 +3,17 @@
 import UIKit
 
 var str = "Hello, playground"
+str = "Hello, Swift"
+let constStr = str
+//constStr = "hello"
+
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+
+var arrayOfInts: Array<Int>
+var arrayOfInts2: [Int]
+
+var dictOfCapitalsByCountry: Dictionary<String,String>
+var dictOfCapitalsByCountry2: [String:String]
+
